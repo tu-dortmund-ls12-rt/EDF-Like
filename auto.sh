8cacbb39623600797f33e115f6ef9a21be0dcc59
marioguenzel <mario.guenzel@tu-dortmund.de>
2021-02-26 12:59:11 +0100

    auto.sh

---
+++ auto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to obtain the Paper results automatically.
+
+python3 main.py 1
+python3 main.py 2
+python3 main.py 3
+python3 main.py 4
+python3 main.py 5a
+python3 main.py 5b
+python3 main.py 6a
+python3 main.py 6b
