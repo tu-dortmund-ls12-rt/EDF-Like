1514d3ffca0385045174027a26a7ff6633a53f90
Mario Günzel <mario@Air-von-Mario.fritz.box>
2022-01-26 15:04:21 +0100

    plot functionality

--- effsstsPlot/effsstsPlot.py
+++ effsstsPlot/effsstsPlot.py
@@ -209,7 +209,7 @@ def effsstsPlot(prefix, plotall, schemes, minsstype, maxsstype, ssofftypes, usta
     fig = plt.figure()
     # create a virtual outer subsplot for putting big x-ylabel
     ax = fig.add_subplot(111)
-    fig.subplots_adjust(top=0.8, left=0.2, right=0.95, bottom=0.2, hspace=0.3)
+    fig.subplots_adjust(top=0.8, left=0.2, right=0.95, bottom=0.25, hspace=0.3)
 
     ax.set_xlabel('Utilization (%)', size=23)
     ax.set_ylabel('Acceptance Ratio', size=23)
